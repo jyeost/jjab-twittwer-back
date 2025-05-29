c5d77f309d11e470972e1b64ef5bbd78d77fa92f
fhwang98 <fhwang9815@gmail.com>
2025-05-29 20:48:23 +0900

    피드 조회 기능 추가

--- src/main/java/jjabtwitter/post/repository/PostRepository.java
+++ src/main/java/jjabtwitter/post/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package jjabtwitter.post.repository;
 
+import jjabtwitter.member.domain.Member;
 import jjabtwitter.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByMember(Member member);
+
+    List<Post> findByMemberIn(List<Member> members);
 }
